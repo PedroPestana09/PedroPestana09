d698d1d8509a85c676ab2c9ea34cd30c40c59fe6
PedroPestana09 <pestana.pedro@outlook.pt>
2024-06-04 15:58:04 +0100

    Delete teste2.py

--- teste2.py
+++
@@ -1,5 +0,0 @@
-if 0 and 1:
-    print(True and 1)  # Não será exibido nada porque a expressão acima tem um Falsy (0 zero)
-
-if 1 and 1:
-    print(True and 1 and 0.0) # Será exibido 0.0 porque é o Falsy na expressão
