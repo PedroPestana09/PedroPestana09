aaa42b82a8c350dd8df17004fca5078583e9992a
PedroPestana09 <pestana.pedro@outlook.pt>
2024-06-04 15:07:59 +0100

    Delete aula10.py

--- aula10.py
+++
@@ -1,7 +0,0 @@
-concatenacao = "A" + "B" + "C"  # Concatenação é a junção de várias strings (apenas strings), usando o operador "+"
-print(concatenacao)
-
-a_dez_vezes = "a" * 10  # Multiplicação usando um numero e uma string
-tres_vezes_pedro = 3 * "Pedro"
-print(a_dez_vezes)
-print(tres_vezes_pedro)
