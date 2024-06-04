40e3a6d5f56aea25b63e586fe539acf617e89319
PedroPestana09 <pestana.pedro@outlook.pt>
2024-06-04 15:08:26 +0100

    Delete aula14.py

--- aula14.py
+++
@@ -1,8 +0,0 @@
-# Formatar usando a função Format
-
-a = "AAAA"
-b = "B"
-c = 1.1
-string = "b={nome2}  a={0} b={0} b={nome2} c={nome3:.2f}"
-formato = string.format(a, nome2=b, nome3=c)  # nome2 e nome3 são chamados de parametros nomeados
-print(formato)
